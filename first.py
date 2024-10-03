3209cc419f1697254fe9b8fcb7f78a6ef6d330ad
NovisBuce <144156059+NovisBuce@users.noreply.github.com>
2024-10-03 08:51:14 +0000

    Druhý program

--- first.py
+++ first.py
@@ -1,4 +1,11 @@
 def hello():
     print("Hello World!")
 
-hello()
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print("Číslo", cislo, "je sudé")
+    else:
+        print("Číslo", cislo, "je liché")
+
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
--- five:stars.py
+++ five:stars.py
@@ -1,4 +1,4 @@
-
+# funkce demonstrující cyklus while
 def stars_while():
     print('zacatek')
 
@@ -10,6 +10,8 @@ def stars_while():
 
     print('konec')
 
+
+# funkce demonsturjící cyklus for
 def stars_for():
     print('zacatek')
 
@@ -18,11 +20,19 @@ def stars_for():
 
     print('konec')
 
+
+# funkce urcujici, zda lezi mezi min_number a max_number
 def in_range(min_number, max_number, number):
     if number <= max_number and number >= min_number:
         print('Is in range')
     else:
         print('Is not in range')
 
-in_range(100, 1000, 1)
+#in_range(100, 1000, 1)
 
+#funkce vypise "Ahoj jmeno"
+def zobraz_pozdrav(jmeno):
+    print("Ahoj", jmeno)
+
+jm = input("Zadej jméno: ")
+zobraz_pozdrav(jm)
