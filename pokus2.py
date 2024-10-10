992820f35ab23598e57c83890ac67587cca1d8d4
NovisBuce <144156059+NovisBuce@users.noreply.github.com>
2024-10-10 09:06:11 +0000

    Pokusy

--- pokus2.py
+++ pokus2.py
@@ -1,25 +1,31 @@
 # funkce vrati treti prvek ze seznamu
-def vrat_treti(seznam):
-    if len(seznam) <= 2:
-        return None
-    else:
-        return seznam[2]
+#def vrat_treti(seznam):
+   # if len(seznam) <= 2:
+    #    return None
+    #else:
+    #    return seznam[2]
 
 
 
 # funkce spocita prumer z hodnot v seznamu
 def udelej_prumer(seznam):
-    return None
+    return sum(seznam) / len(seznam)
 
 # funkce naformatuje retezec, aby vratila text ve formatu:
 # "Jmeno: Jan, Prijmeni: Novak, Vek: 20, Prumerna znamka: 2.5"
 def naformatuj_text(slovnik):
-    return ""
+    jmeno = slovnik["jmeno"]
+    prijmeni = slovnik["prijmeni"]
+    vek = slovnik["vek"]
+    return f"Jmeno: {jmeno} Příjmení: {prijmeni} Věk:  {vek} Průměrná známka: {sum(student['znamky'])/len(student['znamky'])}"
 
 
 if __name__ == "__main__":
-    seznam = [9,8]
-    vysledek = vrat_treti(seznam)
+    #seznam = [9,8]
+    #vysledek = vrat_treti(seznam)
+   # print(vysledek)
+    obalka = [9,8,7,6,5]
+    vysledek = udelej_prumer(obalka)
     print(vysledek)
 
 
@@ -30,5 +36,7 @@ if __name__ == "__main__":
         "vek": 21,
         "znamky": [1, 2, 1, 1, 3, 2]
     }
+
+    vysledek = naformatuj_text(student)
     print(naformatuj_text(student))
 
