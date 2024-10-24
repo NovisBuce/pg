268596d509019b9e290b63a16aed5a2f715c2808
NovisBuce <144156059+NovisBuce@users.noreply.github.com>
2024-10-24 08:45:30 +0000

    cviceni4

--- cviceni4_levenstein.py
+++ cviceni4_levenstein.py
@@ -10,8 +10,15 @@ def levensteinova_vzdalenost(dotaz1, dotaz2):
             levenstein += 1
         i += 1
     return levenstein
-        
 
+def levensteinova_vzdalenost2(dotaz1, dotaz2):
+    levenstein = 0
+    lenght = min(len(dotaz1), len(dotaz2))
+    for i in range(lenght):
+        if dotaz1[i] != dotaz2[i]:
+            levenstein += 1
+    levenstein += abs(len(dotaz1) - len(dotaz2))
+    return levenstein
     
 
 if __name__ == "__main__":
@@ -20,6 +27,6 @@ if __name__ == "__main__":
     query2 = "seznamka"
     query3 = "sesnam"
 
-    print(levensteinova_vzdalenost(query1, query2))
-    print(levensteinova_vzdalenost(query2, query3))
-    print(levensteinova_vzdalenost(query1, query3))
+    print(levensteinova_vzdalenost2(query1, query2))
+    print(levensteinova_vzdalenost2(query2, query3))
+    print(levensteinova_vzdalenost2(query1, query3))
