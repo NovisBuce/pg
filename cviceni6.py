28661024a90aa4b13c7309691d5974063f989194
NovisBuce <144156059+NovisBuce@users.noreply.github.com>
2024-11-14 13:06:04 +0000

    Cvičení 6

--- cviceni6.py
+++ cviceni6.py
@@ -8,4 +8,9 @@ data = [
 ]
 
 if __name__ == "__main__":
-    pass
+    with open("data.json", "r") as fp:
+          json_data2 = fp.read()
+          data2 = json.loads(json_data2)
+
+    data2.pop(0)
+print(data2)
