403d473225e1bb88bf2c129c981a518879194d0e
NovisBuce <144156059+NovisBuce@users.noreply.github.com>
2024-12-11 16:40:31 +0000

    Ninth

--- ninth.py
+++ ninth.py
@@ -1,19 +1,24 @@
 def dec_to_bin(cislo):
-   def dec_to_bin(cislo):
     """
     Funkce prevede cislo na binarni reprezentaci (cislo muze byt str i int).
     Vraci binarni reprezentaci jako retezec (str).
     """
-    if isinstance(cislo, str):
+    try:
         cislo = int(cislo)
+        
+        return bin(cislo)[2:]
+    except ValueError:
+        raise ValueError("Vstup musí být číslo nebo řetězec reprezentující číslo.")
 
-    return bin(cislo)[2:]
-
-
-def test_bin_to_dec():
-    assert dec_to_bin("0") == "0"
-    assert dec_to_bin(1) == "1"
-    assert dec_to_bin("100") == "1100100"
-    assert dec_to_bin(101) == "1100101"
-    assert dec_to_bin(127) == "1111111"
-    assert dec_to_bin("128") == "10000000"
+def test_dec_to_bin():
+    """
+    Testy pro funkci dec_to_bin.
+    """
+    assert dec_to_bin("0") == "0"      
+    assert dec_to_bin(0) == "0"       
+    assert dec_to_bin(1) == "1"       
+    assert dec_to_bin("100") == "1100100"  
+    assert dec_to_bin(101) == "1100101"    
+    assert dec_to_bin(127) == "1111111"    
+    assert dec_to_bin("128") == "10000000" 
+    assert dec_to_bin(167) == "10100111"   
